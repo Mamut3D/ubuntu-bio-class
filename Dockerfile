52e194aadedacac97929c4c4e7c369be00096d82
Boris Parak <parak@cesnet.cz>
2016-03-18 17:45:32 +0100

    Refactored repo/script structure

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,22 @@
 FROM ubuntu:14.04
 
-MAINTAINER Radim Janca <janca@ics.muni.cz>
+MAINTAINER MetaCloud Team <cloud@metacentrum.cz>
+LABEL version="1.0" build="Fri Mar 18 17:30:22 CET 2016"
 
-# FedCloud stuff
-ADD InstallScript.sh /tmp/InstallScript.sh
-RUN /tmp/InstallScript.sh
+# Copy installation scripts
+ADD install_* /tmp
+ADD install_bioconductor.r /opt
 
-# Expose RStudio web default port
-EXPOSE 8787
+# Install software
+RUN /tmp/install_repos.sh
+RUN /tmp/install_sshd.sh
+RUN /tmp/install_rstudio.sh
+RUN /tmp/install_ext.sh
+RUN /tmp/install_users.sh
+RUN /tmp/install_finalize.sh
 
-#Run server
-CMD ["/usr/lib/rstudio-server/bin/rserver","--server-daemonize","0"]
+# Expose RStudio + SSH
+EXPOSE 8787 22
 
+# Start rstudio-server by default
+CMD ["/usr/lib/rstudio-server/bin/rserver", "--server-daemonize", "0"]
---
+++ install_rstudio.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------- #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
+source $(dirname $0)/install_common.sh
+
+# Install dependencies
+apt-get -y -qq install wget git libc6-dev cpp gcc g++ zlib1g-dev devscripts build-essential \
+                       rpm2cpio cpio libgstreamer-plugins-base0.10-0 libgstreamer0.10-0 libjpeg62 \
+                       liborc-0.4-0 libxslt1-dev libedit2 curl libcurl4-openssl-dev
+
+# Install R
+apt-get -y -qq install r-base r-base-dev
+
+# Install RStudio and RStudio-server
+wget https://download1.rstudio.org/rstudio-0.99.892-amd64.deb -P /var/cache/apt/archives/
+wget https://download2.rstudio.org/rstudio-server-0.99.892-amd64.deb -P /var/cache/apt/archives/
+apt-get -y -qq install rstudio rstudio-server
+
+# Configure RServer to start in an environment without AppArmor
+echo "server-app-armor-enabled=0" >> /etc/rstudio/rserver.conf
